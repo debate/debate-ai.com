5e28fbe1b2cd479d0ebf6dceb463323f38a55b41
vtempest <1274452+vtempest@users.noreply.github.com>
2026-03-02 09:33:56 -0800

    fix

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import { Suspense } from "react"
 import { DebateVideosPage } from "@/components/debate/videos/DebateVideosPage"
 
 export const metadata: Metadata = {
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 }
 
 export default function Home() {
-  return <DebateVideosPage />
+  return (
+    <Suspense>
+      <DebateVideosPage />
+    </Suspense>
+  )
 }
